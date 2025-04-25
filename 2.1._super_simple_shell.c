7c42be553356b33b66b880b52aed534483247c4c
Bevan Tan <btan400@gmail.com>
2025-04-25 00:27:26 +0000

    2.1._super_simple_shell.c

--- 2.1._super_simple_shell.c
+++ 2.1._super_simple_shell.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 /**
  *
  *
@@ -10,60 +11,44 @@
  */
 int main(int ac, char **av)
 {
-	int count;
-	int max;
 	int child_pid;
-	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
-	int status;
-	int my_pid;
-	int my_ppid;
 	char *buffer;
+	char *argv[] = {&buffer, NULL};
+	int status;
 	size_t buffer_size;
 	size_t line;
 
-	count = 0;
-	max = 5;
+	buffer = NULL;
+	buffer_size = 0;
 
-	while (count < max)
+	while (1)
 	{
-		my_pid = getpid();
-		printf("is this the parent: %d\n", my_pid);
-
 		printf("$ ");
 
 		child_pid = fork();
 		if (child_pid == 0)
 		{
 			line = getline(&buffer, &buffer_size, stdin);
+			if (buffer == NULL)/*buffer is malloced*/
+			{
+				return (-1);
+			}
+
 			printf("line is: %zu", line);
-			/*split*/
-
 			/*save these into argv*/
-
-			my_pid = getpid();
-			my_ppid = getppid();
-
-			printf("Parent pid:%d\n", my_ppid);
-			printf("Child pid:%d\n", my_pid);
-			printf("%d\n", count);
 
 			if (execve(buffer, argv, NULL) == -1)
 			{
 				perror("ERROR: exceve error");
 			}
-/*			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("ERROR: exceve error");
-			}
-*/
 			printf("%zu line read\n", line);
 		}
 		else 
 		{
 			wait(&status);
 		}
-		count = count + 1;
 	}
+	free(buffer);
 	return (0);
 }
 
